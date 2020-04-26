3e460a4a03c17e510fc7eb641e93c783e212eaa1
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-26 12:07:37 +0200

    created Home component and trying to render it from server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const React = require('react')
+const renderToString = require('react-dom/server')
+	.renderToString
+const Home = require('./client/components/Home')
+	.default
 const app = express()
+
+app.get('/', (req, res) => {
+	const content = renderToString(<Home />)
+
+	res.send(content)
+})
 
 app.listen(7000, () => {
 	console.log('Listening on port 7000')
