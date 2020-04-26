c989ed0a53975293cd8e4f6d2952cbf334dc5a4c
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-26 12:34:44 +0200

    running succesfully home component via server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const React = require('react')
 const renderToString = require('react-dom/server')
 	.renderToString
-const Home = require('./client/components/Home')
+const Home = require('./client/components/home')
 	.default
 const app = express()
 
