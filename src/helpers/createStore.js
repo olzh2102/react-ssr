62fed07e19c711bbec99c8112388224815ec009b
Olzhas Kurikov <olzhas2102@yahoo.com>
2020-04-27 11:12:51 +0200

    server redux store creation

---
+++ src/helpers/createStore.js
@@ -0,0 +1,15 @@
+import {
+	createStore,
+	applyMiddleware,
+} from 'redux'
+import thunk from 'redux-thunk'
+
+export default () => {
+	const store = createStore(
+		reducers,
+		{},
+		applyMiddleware(thunk)
+	)
+
+	return store
+}
